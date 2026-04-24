3970b2367e888ed39fcbfef66724254b2ba3662c
6peng <liupeng@kylinos.cn>
2026-04-24 17:26:00 +0800

    update

--- backup-mydata.sh
+++ backup-mydata.sh
@@ -3,7 +3,7 @@
 #    USER=<homedir username here>
 #    rsync -aP --exclude-from=rsync-homedir-excludes.txt /home/$USER/ /media/$USER/linuxbackup/home/$USER/
 
-DESTDIR=/media/$USER/mydata/Backups
+DESTDIR=/media/$USER/Fanxiang/Backups/mydata
 MYDATA=/home/$USER/mydata
 
-rsync -arzv --delete --sparse --exclude-from=./rsync-mydata-excludes.txt $MYDATA/ $DESTDIR/mydata/
+rsync -arzv --delete --sparse --exclude-from=./rsync-mydata-excludes.txt $MYDATA/ $DESTDIR/
--- backup-myhome.sh
+++ backup-myhome.sh
@@ -3,6 +3,6 @@
 #    USER=<homedir username here>
 #    rsync -aP --exclude-from=rsync-homedir-excludes.txt /home/$USER/ /media/$USER/linuxbackup/home/$USER/
 
-DESTDIR=/media/$USER/mydata/Backups
+DESTDIR=/media/$USER/Fanxiang/Backups
 
 rsync -arzv --delete --sparse --exclude-from=./rsync-homedir-excludes.txt $HOME/ $DESTDIR/home-$USER/
